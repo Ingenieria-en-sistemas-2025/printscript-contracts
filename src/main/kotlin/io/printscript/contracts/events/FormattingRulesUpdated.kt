889bc0ffa09b7145480c856a0395f0e003dff542
pilarsilveyra <psilveyra@mail.austral.edu.ar>
2025-11-16 23:03:29 -0300

    instant in events

--- src/main/kotlin/io/printscript/contracts/events/FormattingRulesUpdated.kt
+++ src/main/kotlin/io/printscript/contracts/events/FormattingRulesUpdated.kt
@@ -4,4 +4,4 @@ import io.printscript.contracts.formatter.FormatterOptionsDto
 import java.time.Instant
 import java.util.UUID
 
-data class FormattingRulesUpdated(val correlationalId: String, val snippetId: UUID, val language: String, val version: String, val configText: String?, val configFormat: String?, val options: FormatterOptionsDto?, val attempt: Int = 0, val createdAt: Instant = Instant.now(),) : DomainEvent
+data class FormattingRulesUpdated(val correlationalId: String, val snippetId: UUID, val language: String, val version: String, val configText: String?, val configFormat: String?, val options: FormatterOptionsDto?, val createdAt: Instant = Instant.now(),) : DomainEvent
--- src/main/kotlin/io/printscript/contracts/events/LintingRulesUpdated.kt
+++ src/main/kotlin/io/printscript/contracts/events/LintingRulesUpdated.kt
@@ -3,4 +3,4 @@ package io.printscript.contracts.events
 import java.time.Instant
 import java.util.UUID
 
-data class LintingRulesUpdated(val correlationalId: String, val snippetId: UUID, val language: String, val version: String, val configText: String?, val configFormat: String?, val attempt: Int = 0, val createdAt: Instant = Instant.now(),) : DomainEvent
+data class LintingRulesUpdated(val correlationalId: String, val snippetId: UUID, val language: String, val version: String, val configText: String?, val configFormat: String?, val createdAt: Instant = Instant.now(),) : DomainEvent
